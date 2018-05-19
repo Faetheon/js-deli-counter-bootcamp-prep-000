9e9d9db1f7090fbc2d30a201ad4e543cad0f6226
Nathan Hill <nateohill@gmail.com>
2018-05-19 01:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine) {
     for(let i = 0; i < katzDeliLine.length; i++) {
       mine.push(i+1, '. ' + katzDeliLine[i])
     }
-    return "The line is currently: " + mine;
+    return "The line is currently: " + [...mine];
   } else {
     return "The line is currently empty."
   }
