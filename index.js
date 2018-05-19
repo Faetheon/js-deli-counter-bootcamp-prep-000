d502e46259b696873a8a69b8d9ec12bc697067bb
Nathan Hill <nateohill@gmail.com>
2018-05-19 01:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function takeANumber(katzDeliLine, name) {
   let arr = [...katzDeliLine];
   return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    return katzDeliLine.shift();
+  } else {
+    return "There is nobody waiting to be serverd!";
+  }
+}
