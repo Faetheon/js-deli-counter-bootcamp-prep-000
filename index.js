6a141ea274e35d676868d375ebba8183bb77c4db
Nathan Hill <nateohill@gmail.com>
2018-05-19 01:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ function takeANumber(katzDeliLine, name) {
   let arr = [...katzDeliLine];
   return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
+
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var mine = [];
+  if(katzDeliLine.length > 0) {
+    return "The line is currently: "
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      mine.push(katzDeliLine[i]);
+    }
+  } else {
+    return "The line is currently empty."
   }
 }
