c0c5a2e8975fdbab05c4e54772dbe026f15bcdfb
Nathan Hill <nateohill@gmail.com>
2018-05-19 01:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   let arr = [...katzDeliLine];
-  return `Welcome, ${name}. You are number ${arr.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${arr.length} in line.`
 }
